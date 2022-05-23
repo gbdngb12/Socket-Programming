774de1067fe90cbc433ff14a34023b8e47d36f74
gbdngb12 <gbdngb12@naver.com>
2022-05-23 11:47:27 +0900

    ftp.retrbinary 이해완료!!

---
+++ ftp4.py
@@ -0,0 +1,14 @@
+import ftplib
+
+ftp = ftplib.FTP("127.0.0.1")
+ftp.login()
+
+entries = ftp.nlst()
+
+for entry in entries :
+    f = open(entry, "w")
+    res = ftp.retrbinary("RETR "+ entry, f.write("This is file download!!"))
+    print(res)
+    f.close()
+
+ftp.quit()
